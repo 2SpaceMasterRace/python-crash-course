d6d34561bdfc7605f489191cc32e4b858820d597
2SpaceMasterRace <alphanovachrono@gmail.com>
2026-09-01 16:04:26 -0400

    Restructured Project layout

--- projects/hotcache/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from hotcache!")
-
-
-if __name__ == "__main__":
-    main()
--- projects/socketcraft/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from socketcraft!")
-
-
-if __name__ == "__main__":
-    main()
